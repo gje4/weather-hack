f842ba8325ef16fd764aea0eebb393fca29c824b
Nancy Montiel <nancy.montiel@c02w709phtdh.attlocal.net>
2021-01-28 10:28:05 -0600

    added featured product to modal

--- components/auth/LoginView.tsx
+++ components/auth/LoginView.tsx
@@ -69,12 +69,15 @@ const LoginView: FC<Props> = () => {
 
   var productBox = {
     backgroundColor: '#fff',
-    borderRadius: 4,
-    borderWidth: 0.5,
-    borderColor: '#000',
-    padding: 10,
-    margin: 20,
-    overflow: 'scroll',
+    // borderRadius: 4,
+    // borderWidth: 0.5,
+    // borderColor: '#000',
+    textAlign: 'center',
+    // padding: 10,
+    // margin: 20,
+    // overflow: 'scroll',
+    fontSize: 36,
+    fontWeight: 'bold'
   }
 
   var styleModal = {
@@ -121,8 +124,12 @@ const LoginView: FC<Props> = () => {
         <div>
           <section style={productBox}>
             <h1>Featured Products</h1>
-            <picture>result</picture>
-            <p>Test Test Test</p>
+            <img src={'https://cdn11.bigcommerce.com/s-lykxcl4bnx/products/156/images/485/snowboard-googles__24729.1611338718.386.513.jpg'}></img>
+            <img src={'https://cdn11.bigcommerce.com/s-lykxcl4bnx/products/153/images/481/heavy-rain-fish__97224.1611338200.386.513.jpg'}></img>
+            <img src={'https://cdn11.bigcommerce.com/s-lykxcl4bnx/products/149/images/476/mittens__44854.1611329460.386.513.jpg'}></img>
+            <img src={'https://cdn11.bigcommerce.com/s-lykxcl4bnx/products/135/images/461/BC-sunglasses__88092.1611327241.386.513.jpg'}></img>
+            <img src={'https://cdn11.bigcommerce.com/s-lykxcl4bnx/products/142/images/488/classic-raincoat__15533.1611339089.386.513.jpg'}></img>
+            
           </section>
         </div>
       </div>
